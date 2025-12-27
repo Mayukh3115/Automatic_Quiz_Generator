5e4796eb97d46f56fad91d73b850b9761a3ce1ba
Mayukh3115 <mayukhmb2005@gmail.com>
2025-12-27 22:37:25 +0530

    fixed login page

--- quizproject/urls.py
+++ quizproject/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('quiz_app.urls')),
     path('', include('quiz_app.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
+     path(
+        "accounts/login/",
+        LoginView.as_view(redirect_authenticated_user=True),
+        name="login"
+    ),
 ]
